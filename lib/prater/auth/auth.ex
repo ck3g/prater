626e051320869676fa92088fffd4a83d0dc1bfd0
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2018-02-18 13:58:15 +0100

    Use Comeonin to encrypt passwords

--- lib/prater/auth/auth.ex
+++ lib/prater/auth/auth.ex
@@ -15,7 +15,7 @@ defmodule Prater.Auth do
     user = Repo.get_by(User, email: email)
 
     cond do
-      user && user.encrypted_password == password ->
+      user && Comeonin.Bcrypt.checkpw(password, user.encrypted_password) ->
         {:ok, user}
       true ->
         {:error, :unauthorized}
