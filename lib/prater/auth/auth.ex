3fc4ccad94d7d37a2af007da9b0d3f50d7f17cba
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2018-02-18 16:46:42 +0100

    Registration functionality

--- lib/prater/auth/auth.ex
+++ lib/prater/auth/auth.ex
@@ -25,4 +25,8 @@ defmodule Prater.Auth do
   def sign_out(conn) do
     Plug.Conn.configure_session(conn, drop: true)
   end
+
+  def register(params) do
+    User.registration_changeset(%User{}, params) |> Repo.insert()
+  end
 end
