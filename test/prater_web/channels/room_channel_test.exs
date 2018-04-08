128fe68346b60db8404e4dd70c9c6bee7da8e5e1
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2018-04-08 11:13:51 +0200

    Test subscribe and join to the room

---
+++ test/prater_web/channels/room_channel_test.exs
@@ -0,0 +1,38 @@
+defmodule PraterWeb.RoomChannelTest do
+  use PraterWeb.ChannelCase
+
+  alias Prater.Conversation
+  alias PraterWeb.UserSocket
+
+  import Prater.AuthHelpers
+
+  setup do
+    user = create_user("user@example.com")
+    {:ok, room} = Conversation.create_room(user, %{name: "Lobby"})
+
+    token = Phoenix.Token.sign(@endpoint, "user token", user.id)
+    {:ok, socket} = connect(UserSocket, %{"token" => token})
+
+    {:ok, socket: socket, user: user, room: room}
+  end
+
+  test "assigns room_id to the socket after join", %{socket: socket, room: room} do
+    {:ok, _, socket} = subscribe_and_join(socket, "room:#{room.id}", %{})
+
+    assert socket.assigns.room_id == "#{room.id}"
+  end
+
+  test "returns list of messages after join", %{socket: socket, user: user, room: room} do
+    Conversation.create_message(user, room, %{"content" => "Hello from Test"})
+
+    {:ok, messages, _} = subscribe_and_join(socket, "room:#{room.id}", %{})
+
+    messages_template = %{
+      messages: [
+        %{content: "Hello from Test", user: %{username: user.username}}
+      ]
+    }
+
+    assert messages == messages_template
+  end
+end
