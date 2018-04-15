f2c40072a580b0f3fc91305799dcc177a4ae134b
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2018-04-15 10:10:38 +0200

    Welcome page test

---
+++ test/prater_web/features/welcome_page_test.exs
@@ -0,0 +1,13 @@
+defmodule PraterWeb.WelcomePageTest do
+  use PraterWeb.ConnCase
+  use Hound.Helpers
+
+  hound_session()
+
+  test "page has a correct title" do
+    navigate_to("/")
+
+    assert page_title() == "Hello Prater!"
+    assert String.contains?(page_source(), "Welcome to Prater")
+  end
+end
