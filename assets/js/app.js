c1e13b777eeb74cc4932d3ef1925ca11cb11b06a
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2018-05-20 13:00:34 +0200

    Configure Webpack

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,4 @@
-// Brunch automatically concatenates all files in your
-// watched paths. Those paths can be configured at
-// config.paths.watched in "brunch-config.js".
-//
-// However, those files will only be executed if
-// explicitly imported. The only exception are files
-// in vendor, which are never wrapped in imports and
-// therefore are always executed.
+import css from "../css/app.scss"
 
 // Import dependencies
 //
