07bee744151e93c9e86d99102f26d270d54f0dbc
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2018-05-27 12:29:59 +0200

    Configure Credo for the project

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule Prater.Mixfile do
       {:cowboy, "~> 1.0"},
       {:comeonin, "~> 4.0"},
       {:bcrypt_elixir, "~> 1.0"},
-      {:hound, "~> 1.0"}
+      {:hound, "~> 1.0"},
+      {:credo, "~> 0.9.1", only: [:dev, :test], runtime: false}
     ]
   end
 
